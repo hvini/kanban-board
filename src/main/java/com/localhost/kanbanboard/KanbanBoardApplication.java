80706bf3f546b3229b3aa7b9ca3398e8795a1d2f
Vinícius Cavalcanti <vhpcavalcanti@outlook.com>
2020-08-19 11:06:29 -0300

    commit inicial

---
+++ src/main/java/com/localhost/kanbanboard/KanbanBoardApplication.java
@@ -0,0 +1,13 @@
+package com.localhost.kanbanboard;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class KanbanBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localhost/kanbanboard/KanbanBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localhost.kanbanboard;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class KanbanBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
