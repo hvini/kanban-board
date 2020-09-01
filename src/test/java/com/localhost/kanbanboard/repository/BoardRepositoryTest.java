5a7ed47a861e2b1d6ea2cdde849e5a50db494922
Vinícius Cavalcanti <vhpcavalcanti@outlook.com>
2020-09-01 19:54:35 -0300

    reescrita dos testes de repositorio do quadro

--- src/test/java/com/localhost/kanbanboard/repository/BoardRepositoryTest.java
+++ src/test/java/com/localhost/kanbanboard/repository/BoardRepositoryTest.java
@@ -5,14 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.localhost.kanbanboard.entity.BoardEntity;
-import com.localhost.kanbanboard.entity.RoleEntity;
-import com.localhost.kanbanboard.entity.UserEntity;
 import org.junit.jupiter.api.BeforeEach;
 import javax.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import java.util.Optional;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * BoardRepositoryTest
@@ -22,35 +18,13 @@ import java.util.Map;
 public class BoardRepositoryTest {
     @Autowired
     private BoardRepository boardRepository;
-    @Autowired
-    private RoleRepository roleRepository;
-    @Autowired
-    private UserRepository userRepository;
     private BoardEntity board;
-    private RoleEntity role;
-    private UserEntity user;
-    private Map<UserEntity, RoleEntity> boardRole;
 
     @BeforeEach
     public void setUp() {
-        user = new UserEntity();
-        user.setFullName("Vinícius Cavalcanti");
-        user.setEmail("vhpcavalcanti@outlook.com");
-        user.setPassword("abc");
-        user.setIsEnabled(false);
-        userRepository.save(user);
-
-        role = new RoleEntity();
-        role.setName("admin");
-        roleRepository.save(role);
-
-        boardRole = new HashMap<UserEntity, RoleEntity>();
-        boardRole.put(user, role);
-        
         board = new BoardEntity();
         board.setName("teste");
         board.setIsFavorite(false);
-        board.setBoardRole(boardRole);
     }
 
     @Test
